4921d225f426b9ce81c75404fa80789a93967091
Paras Shenmare <shenmareparas@gmail.com>
2025-02-25 00:09:30 +0530

    Portrait Lock

--- app/src/main/java/com/example/music/MainActivity.kt
+++ app/src/main/java/com/example/music/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.music
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,6 +16,9 @@ import com.example.music.ui.theme.MusicTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
         WindowCompat.setDecorFitsSystemWindows(window, false)
         val exoPlayer = ExoPlayer.Builder(this).build()
         val repository = MusicRepository(Network.api, exoPlayer)
